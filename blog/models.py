ec4d09b2ae154170a5163601fd6eb286c018ae6b
swathi keshavamurthy <swathik.2197@gmail.com>
2024-03-24 20:11:04 +0000

    Create Post model and makemigrations blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Constants for the Post model choices
+STATUS = (
+    (0, "Draft"),
+    (1, "Published"),
+)
+
+CATEGORY_CHOICES = [
+    ('front-end development', 'Front-End Development'),
+    ('e-commerce', 'E-Commerce'),
+    ('predictive analytics', 'Predictive Analytics'),
+]
+
+class Post(models.Model):
+    """
+    Model representing a blog post.
+    """
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='front-end development')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    approved = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name="likes_post", blank=True)
+    bookmarks = models.ManyToManyField(User, related_name="bookmarks_post", blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def total_likes(self):
+        """
+        Returns the total number of likes for the post.
+        """
+        return self.likes.count()
+
+    def total_bookmarks(self):
+        """
+        Returns the total number of bookmarks for the post.
+        """
+        return self.bookmarks.count()

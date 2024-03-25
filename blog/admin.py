1a8315f363994a1fc0819ebda8a5b4e879355d90
swathi keshavamurthy <swathik.2197@gmail.com>
2024-03-25 00:16:48 +0000

    Update Blog posts using summernote and styling post

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    Lists fields for display in admin, fields for search,
+    field filters, fields to prepopulate, and rich-text editor.
+    """
+
+    list_display = ('title', 'slug', 'author', 'status', 'created_on', 'updated_on',)
+    search_fields = ['title', 'author__username']  # Added 'author__username' for searching by author's username
+    list_filter = ('status', 'created_on', 'updated_on', 'author__username',)  # Added 'author__username' for filtering by author's username
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 admin.site.register(Comment)
--- softwarestacks/settings.py
+++ softwarestacks/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-swathikesha-softwaresta-yuwx4g4ondz.ws-eu110.gitpod.io',
                 '.herokuapp.com']
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_summernote',.
+    'django_summernote',
     'blog',
 ]
 

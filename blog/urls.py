099e18cf259fd0aa7091ad02525ebc950359839a
swathi keshavamurthy <swathik.2197@gmail.com>
2024-03-27 10:19:50 +0000

    Added a corresponding URL pattern

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from . import views
 from django.urls import path
-from .views import category_posts
+from .views import category_posts, bookmarked_posts
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_name>/', category_posts, name='category_posts'),
+    path('my-bookmarks/', bookmarked_posts, name='bookmarked_posts'),
 ]

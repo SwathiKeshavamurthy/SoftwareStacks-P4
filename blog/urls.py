f20ea61e9ca79d63ec8d7d264b4d33d9994de562
swathi keshavamurthy <swathik.2197@gmail.com>
2024-03-27 10:37:16 +0000

    Added a corresponding URL pattern for liked_posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from . import views
 from django.urls import path
-from .views import category_posts, bookmarked_posts
+from .views import category_posts, bookmarked_posts, liked_posts
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('my-bookmarks/', bookmarked_posts, name='bookmarked_posts'),
+    path('my-likes/', liked_posts, name='liked_posts'),
     path('category/<slug:category_name>/', category_posts, name='category_posts'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
